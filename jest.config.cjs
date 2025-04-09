41b674035d9b07d8d7220eda227fee512aef0803
Christian José Torrealba Rondón <torrealba.christian@gmail.com>
2025-04-10 01:32:40 +0200

    chore: :wrench: Configure jest environment for testing

--- jest.config.cjs
+++ jest.config.cjs
@@ -3,10 +3,16 @@ module.exports = {
   testEnvironment: "jsdom",
   setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
   moduleNameMapper: {
+    "^@src/(.*)$": "<rootDir>/src/$1",
     "\\.(css|less|scss|sass)$": "identity-obj-proxy",
   },
   transform: {
     "^.+\\.(ts|tsx)$": "ts-jest",
   },
   testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
+  globals: {
+    "ts-jest": {
+      tsconfig: "<rootDir>/tsconfig.app.json",
+    },
+  },
 };
